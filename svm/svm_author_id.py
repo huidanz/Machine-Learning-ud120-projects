80eaa154a308e30b138f12e447c09f1e7e4a44d8
huidanz <31378442+huidanz@users.noreply.github.com>
2018-01-20 18:46:44 +0800

    Complete the original SVM mini-project

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -25,6 +25,21 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+clf = svm.SVC(kernel="linear")
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t1 = time()
+pred = clf.predict(features_test)
+pred
+print "predicting time:", round(time()-t1, 3), "s"
+
+print accuracy_score(labels_test, pred)  
+    
+    
 #########################################################
 
 
