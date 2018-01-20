c656059c7714dce121418eadc3702dfdd1dc82e0
huidanz <31378442+huidanz@users.noreply.github.com>
2018-01-20 19:05:06 +0800

    Reduce the dataset size to 1% and swith the SVM kernel to 'rbf'

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -27,7 +27,11 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 from sklearn import svm
 from sklearn.metrics import accuracy_score
-clf = svm.SVC(kernel="linear")
+clf = svm.SVC(kernel="rbf")
+# cut the dataset to 1%
+features_train = features_train[:len(features_train)/100]
+labels_train = labels_train[:len(labels_train)/100]
+
 t0 = time()
 clf.fit(features_train, labels_train)
 print "training time:", round(time()-t0, 3), "s"
